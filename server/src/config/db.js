9751f172f697bff3ad07ee3b90e9ef6e4b9244c8
Cho Won Jae <yoteamo7@naver.com>
2022-05-12 00:04:22 +0900

    db host 변경

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === "dev") {
     .catch((err) => logger.error(err.message));
 } else {
   mongoose
-    .connect(process.env.DB_HOST, {
+    .connect(process.env.DB_HOST_TEST, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
     })
