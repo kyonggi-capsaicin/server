2f7c67e3fa9c7a5f3a4f5bce38d48c817001b740
Cho Won Jae <yoteamo7@naver.com>
2022-05-11 21:22:09 +0900

    Docker 배포 구축

--- server/src/utils/redis.js
+++ server/src/utils/redis.js
@@ -1,12 +1,26 @@
 import { createClient } from "redis";
 
 let client;
-(async () => {
-  client = createClient();
 
-  client.on("error", (err) => console.log("Redis Client Error", err));
+if (process.env.NODE_ENV === "dev") {
+  (async () => {
+    client = createClient();
 
-  await client.connect();
-})();
+    client.on("error", (err) => console.log("Redis Client Error", err));
+
+    await client.connect();
+  })();
+} else {
+  (async () => {
+    client = createClient({
+      host: "redis-server",
+      port: REDIS_PORT_PRO,
+    });
+
+    client.on("error", (err) => console.log("Redis Client Error", err));
+
+    await client.connect();
+  })();
+}
 
 export default client;
